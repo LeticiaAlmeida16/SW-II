616dd4efc66f6c87780a485e5817019ad16f786a
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2025-02-11 12:42:56 -0300

    aula01

--- aula01/calculamedia.php
+++ aula01/calculamedia.php
@@ -7,11 +7,11 @@ echo("Nota 1: " . $nota1 . "<br>");
 
 echo("Nota 2: " . $nota2 . "<br>");
 
-echo("Nota 3: " . $nota3 . "<br>");
+echo("Nota 3: " . $nota3 . "<br> <br>");
 
 $media = ($nota1 + $nota2 + $nota3)/3;
 
-echo("Media: " . $media . ".  ");
+echo("Media: " . $media . ". ");
 
 if ($media >= 6){
     echo "Aprovado!";
