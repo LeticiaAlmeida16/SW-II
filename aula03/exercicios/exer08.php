e051359adfe1beff9b5b21d5796ba237c59d3e5a
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2025-03-10 20:32:00 -0300

    finalizado

--- aula03/exercicios/exer08.php
+++ aula03/exercicios/exer08.php
@@ -1,4 +1,13 @@
 <?php
+    function NumAleatorios() {
+        $array = [];
 
+        for ($i = 0; $i < 10; $i++) {
+            $array[] = rand(1, 100); 
+        }
 
+        return $array;
+    }
+    $nums = NumAleatorios();
+    print_r($nums);
 ?>
